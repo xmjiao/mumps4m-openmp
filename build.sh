2722aaf44058a9a0e4f06b0009bf8d31d808ee8e
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2022-11-15 15:02:46 -0500

    Compiled on Linux

--- build.sh
+++ build.sh
@@ -4,45 +4,62 @@ set -e
 
 export SYSTEM=$(uname -s | cut -d'_' -f1)
 export MACHINE=x86_64
-
-METIS_VERSION=5.1.0
 export MUMPS_VERSION=5.3.4
 export MUMPS_MAJOR_VERSION=$(echo ${MUMPS_VERSION} | cut -d'.' -f1)
+
+OPENBLAS_VERSION=0.3.21
+METIS_VERSION=5.1.0
+
 MAKE=$([ "${SYSTEM}" = 'MINGW64' ] && echo 'mingw32-make' || echo 'make')
 PREFIX=$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
 
 # Force to use x86_64 on Mac with Apple silicon
 ARCH=$([ "${SYSTEM}" = 'Darwin' -a "$(uname -m)" = 'arm64' ] && echo 'arch -x86_64' || true)
 
-build_metis_64() {
+build_openblas() {
     # Download and unpack
-    curl -s -L http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz | tar zxf -
+    curl -Ls https://github.com/xianyi/OpenBLAS/archive/v${OPENBLAS_VERSION}.tar.gz | tar zxf -
+    cd OpenBLAS-${OPENBLAS_VERSION}
+
+    # Build static library
+    make INTERFACE64=0 NO_LAPACKE=1 NO_CBLAS=1 NO_SHARED=1 USE_OPENMP=1 NUM_PARALLEL=$(nproc)
+
+    # Copy static library
+    mkdir -p "${PREFIX}/lib/${SYSTEM}-${MACHINE}"
+    cp libopenblas.a "${PREFIX}/lib/${SYSTEM}-${MACHINE}"
+    cd $PREFIX
+}
+
+build_metis() {
+    # Download and unpack
+    curl -Ls http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz | tar zxf -
     cd metis-${METIS_VERSION}
 
     # Build METIS static library
-    if [ "${SYSTEM}" = "Linux" ]; then
-        perl -e 's/#define IDXTYPEWIDTH 32/#define IDXTYPEWIDTH 64/g' -pi include/metis.h
-    else
-        perl -e 's/#define IDXTYPEWIDTH 64/#define IDXTYPEWIDTH 32/g' -pi include/metis.h
-    fi
     MACVER=$([ "${SYSTEM}" = 'Darwin' -a "$(uname -m)" = 'arm64' ] && echo 'CC=gcc CFLAGS=-mmacosx-version-min=10.15' || true)
     ${ARCH} ${MAKE} ${MACVER} config
     ${ARCH} ${MAKE}
 
-    # Copy header and static library
-    mkdir -p "${PREFIX}/lib/${SYSTEM}-${MACHINE}" "${PREFIX}/include/${SYSTEM}-${MACHINE}"
+    # Copy static library
+    mkdir -p "${PREFIX}/lib/${SYSTEM}-${MACHINE}" "${PREFIX}/include"
     cp build/${SYSTEM}-x86_64/libmetis/libmetis.a "${PREFIX}/lib/${SYSTEM}-${MACHINE}"
-    cp include/metis.h "${PREFIX}/include/${SYSTEM}-${MACHINE}"
+    cp include/metis.h "${PREFIX}/include"
     cd $PREFIX
 }
 
 cp Makefile-${SYSTEM}.inc MUMPS/Makefile.inc
 cp src/Makefile MUMPS/src/Makefile
+cp libseq/Makefile MUMPS/libseq/Makefile
+cp MATLAB/make.inc MUMPS/MATLAB/make.inc
 
-# Build metis with 64-bit integer
-[ -f "$PREFIX/include/${SYSTEM}-${MACHINE}/metis.h" -a -f "$PREFIX/lib/${SYSTEM}-${MACHINE}/libmetis.a" ] || build_metis_64
+# Build openblas
+[ -f "$PREFIX/lib/${SYSTEM}-${MACHINE}/libopenblas.a" ] || build_openblas
+
+# Build metis
+[ -f "$PREFIX/include/metis.h" -a -f "$PREFIX/lib/${SYSTEM}-${MACHINE}/libmetis.a" ] || build_metis
 
 # Build shared objects for single-precision real and complex arithemetic
+${ARCH} ${MAKE} -C MUMPS/libseq clean
 ${ARCH} ${MAKE} -C MUMPS clean
 ${ARCH} ${MAKE} -C MUMPS s sexamples
 (cd MUMPS/examples; ./ssimpletest < input_simpletest_real)
@@ -54,3 +71,7 @@ ${ARCH} ${MAKE} -C MUMPS d dexamples
 (cd MUMPS/examples; ./dsimpletest < input_simpletest_real)
 ${ARCH} ${MAKE} -C MUMPS z zexamples
 (cd MUMPS/examples; ./zsimpletest < input_simpletest_cmplx)
+
+# Build MATLAB and run tests
+${ARCH} ${MAKE} -C MUMPS/MATLAB clean d z
+(cd MUMPS/MATLAB; matlab -nodisplay -batch 'simple_example; zsimple_example')
